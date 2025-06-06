ab404fe0030d553b307199a8989ae6ccd501e9da
Aya7m <yyoyo6987@gmail.com>
2025-06-06 12:12:42 +0300

    editFooter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,18 +3,10 @@ const Footer = () => {
 		<footer className='md:px-8 md:py-0 border-t'>
 			<div className='container flex  items-center gap-4 h-24'>
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
-					Built by{" "}
-					<a
-						href='https://twitter.com/shadcn'
-						target='_blank'
-						rel='noreferrer'
-						className='font-medium underline underline-offset-4'
-					>
-						you
-					</a>
+				
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/shadcn-ui/ui'
+						href='https://github.com/Aya7m/prismaProgect-only-hors'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
